f8baec3c69ec0db3d0945fd68478cd7aca2b3ccd
Caston Lambert <cdlambert33@yahoo.com>
2022-08-08 15:27:18 -0500

    cleaned up index view

--- catalog/views.py
+++ catalog/views.py
@@ -39,26 +39,13 @@ def index(request):
         count = genres[i]['count']
         values = [name,count]
         genre_counts[i] = dict(zip(keys, values))
-    
-
-    num_scifi = Book.objects.filter(genre__name__exact='Science Fiction').count()
-    num_fantasy = Book.objects.filter(genre__name__exact='Fantasy').count()
-    num_nonfi = Book.objects.filter(genre__name__exact='Nonfiction').count()
-    num_comics = Book.objects.filter(genre__name__exact='Comics').count()
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
-        'num_scifi': num_scifi,
-        'num_fantasy': num_fantasy,
-        'num_nonfi': num_nonfi,
-        'num_comics': num_comics,
-        'num_genre': num_genre,
-        'genres': genres,
         'genre_counts': genre_counts,
-        'range': (1,total_genre_count),
     }
 
     # render the HTML template index.html with the dat in the context variable
