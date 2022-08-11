53ec66eebfc3a19a5c7fe5a27472271f9898c9b2
Caston Lambert <cdlambert33@yahoo.com>
2022-08-11 13:54:56 -0500

    added author and book list and detail views

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,9 @@
 from itertools import count
 from django.db.models import Count
 from django.shortcuts import render
+from django.views import generic
 from .models import Book, Author, BookInstance, Genre
+
 
 # Create your views here.
 
@@ -17,17 +19,13 @@ def index(request):
     # the 'all()' is implied by default
     num_authors = Author.objects.count()
 
-  
-
-    # get count of each genre
+    # get genre names and counts
     num_genre = Genre.objects.all().values('name')
     genre_names = []
     for i in num_genre:
         genre_names.append(i['name'])
 
-    for i in num_genre:
-        genre_names.append(i)
-
+    # gets the total count of each genre grouped by genre name
     genres = Book.objects.all().order_by('genre').values('genre').annotate(count=Count('genre'))
 
     total_genre_count = len(num_genre)
@@ -48,5 +46,30 @@ def index(request):
         'genre_counts': genre_counts,
     }
 
-    # render the HTML template index.html with the dat in the context variable
+    # render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(AuthorDetailView, self).get_context_data(**kwargs)
+
+        # Create any data and add it to the context
+        books = Book.objects.all()
+        context['books'] = books
+
+        return context
